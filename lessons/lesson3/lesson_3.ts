6964febdff79a74a97d3466886c212135e077ae1
Oleg <9helgi9gmail.com>
2022-11-20 20:03:10 +0100

    completed lesson 3

--- lessons/lesson3/lesson_3.ts
+++ lessons/lesson3/lesson_3.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 
 console.log('lesson 3');
@@ -16,7 +17,39 @@ console.log('lesson 3');
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
+axios.get("https://jsonplaceholder.typicode.com/posts/1").then((res) => {
+    console.log(res.data)
+})
+axios.post("https://jsonplaceholder.typicode.com/posts",{
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'Star Wars',
+        body: 'Film',
+        userId: 1,
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => console.log(response.data.body))
+axios.put("https://jsonplaceholder.typicode.com/posts/1", {
+    method: 'PUT',
+    body: JSON.stringify({
+        id: 1,
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+}).then((response) => {
+    console.log(response.data.body)
+})
 
+axios.delete("https://jsonplaceholder.typicode.com/posts/1", {
+    method: 'DELETE'
+})
 
 // just a plug
 export default ()=>{};
